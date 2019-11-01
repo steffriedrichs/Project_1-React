3d9b55523a4d8a1e9d3acf0d9b27d69d989693ba
Steffi Friedrichs <steffriedrichs@Steffis-MacBook-Pro-2.local>
2019-11-01 14:47:12 +0100

    Add stones to game

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { PlayingBoard, Field } from "./style";
+import { PlayingBoard, Field, Stone } from "./style";
 
 export default class Board extends Component {
     constructor(props) {
@@ -10,11 +10,32 @@ export default class Board extends Component {
 
     getFields(fieldsArray, rows) {
         return fieldsArray.map((field, index) => (
-            <Field key={index} id={index} rows={rows} dark={field}>{index}</Field>
+            <Field key={index} id={index} rows={rows} dark={field}>
+                <Stone key={index} id={index} rows={rows}>{index}</Stone>
+            </Field>
         ));
     }
 
+    initStones(n) {
+        let stonesArray = [...Array(n * n).fill(0)];
+        let numbersToSelect = Math.floor(n * n * 0.4);
+
+        for (let i = 0; i < numbersToSelect; i = i + 1) {
+            const index = Math.floor(Math.random() * n * n);
+            if (stonesArray[index] === 0) {
+                stonesArray[index] = 1;
+            } else {
+                numbersToSelect = numbersToSelect + 1;
+            }
+        }
+        return stonesArray;
+    }
+
     render() {
-        return <PlayingBoard>{this.getFields(this.props.fieldsArray, this.props.numberOfRows)}</PlayingBoard>;
+        let currentStones = this.initStones(this.props.numberOfRows);
+        console.log(currentStones);
+        return <PlayingBoard>
+            {this.getFields(this.props.fieldsArray, this.props.numberOfRows)}
+        </PlayingBoard>;
     }
 }
--- src/components/Board/style.js
+++ src/components/Board/style.js
@@ -16,9 +16,22 @@ export const Field = styled.div`
     max-width: ${props => 400 / props.rows - props.rows}px;
     max-height: ${props => 400 / props.rows - props.rows}px;
     width: ${props => 60 / props.rows - 1}vw;
-    line-height: ${props => 60 / props.rows - 1}vw;
+    height: ${props => 60 / props.rows - 1}vw;
     float: left;
     margin: 2px 0px 0px 2px;
     border-radius: 3px;
     color: white;
+`;
+
+export const Stone = styled.div`
+    background: black;
+    max-width: ${props => 200 / props.rows - props.rows}px;
+    max-height: ${props => 200 / props.rows - props.rows}px;
+    width: ${props => 30 / props.rows - 1}vw;
+    line-height: ${props => 30 / props.rows - 1}vw;
+    float: left;
+    margin: 2px 0px 0px 2px;
+    border-radius: 100px;
+    color: black;
+    z-index: 2;
 `;

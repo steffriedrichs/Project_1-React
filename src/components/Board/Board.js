da28f61054ee9dd2ca83e507c72924d48e8eb2e2
Steffi Friedrichs <steffriedrichs@Steffis-MBP-2.fritz.box>
2019-11-17 16:54:17 +0100

    Add mouse events to detect swipe direction

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -23,9 +23,32 @@ export default class Board extends Component {
         return stonesArray;
     }
 
+    handleMouseMove(mouseMoveEvent, mouseDown) {
+        if (!mouseDown) return null;
+
+        let moveX = mouseMoveEvent.movementX
+        let moveY = mouseMoveEvent.movementY
+        let result;
+
+        console.log("mouseDown", mouseDown);
+
+        if (Math.abs(moveX) > Math.abs(moveY)) {
+            result = moveX > 0 ? "right" : "left";
+        }
+        if (Math.abs(moveY) > Math.abs(moveX)) {
+            result = moveY > 0 ? "down" : "up";
+        }
+        console.log(result);
+        return result;
+    }
+
     getFields(fieldsArray, rows, stones) {
+        let mouseDown = false;
         return fieldsArray.map((field, index) => (
-            <Field key={index} id={index} rows={rows} dark={field}>
+            <Field key={index} id={index} rows={rows} dark={field}
+                onMouseUp={() => mouseDown = false}
+                onMouseDown={() => mouseDown = true}
+                onMouseMove={mouseMoveEvent => this.handleMouseMove(mouseMoveEvent, mouseDown)}>
                 <Stone rows={rows} exists={stones[index]}></Stone>
             </Field>
         ));
@@ -33,9 +56,20 @@ export default class Board extends Component {
 
     render() {
         let currentStones = this.initStones(this.props.numberOfRows);
-        console.log(currentStones);
         return <PlayingBoard>
             {this.getFields(this.props.fieldsArray, this.props.numberOfRows, currentStones)}
         </PlayingBoard>;
     }
 }
+
+
+{/* 
+onTouchStart={touchStartEvent => this.handleTouchStart(touchStartEvent)}
+onTouchMove={touchMoveEvent => this.handleTouchMove(touchMoveEvent)}
+onTouchEnd={() => this.handleTouchEnd()}
+
+onMouseDown={mouseDownEvent => this.handleMouseDown(mouseDownEvent)}
+onMouseMove={mouseMoveEvent => this.handleMouseMove(mouseMoveEvent)}
+onMouseUp={() => this.handleMouseUp()}
+onMouseLeave={() => this.handleMouseLeave()}
+*/}

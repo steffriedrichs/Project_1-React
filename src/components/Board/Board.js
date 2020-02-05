f0dc51ec21fd4fa2e1879835f684ab2ae2b5ec80
Steffi Friedrichs <steffriedrichs@Steffis-MacBook-Pro-2.local>
2020-02-05 18:29:08 +0100

    Prettier setup

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -36,8 +36,8 @@ export default class Board extends Component {
     getMouseMove(mouseMoveEvent, mouseDown) {
         if (!mouseDown) return null;
 
-        let moveX = mouseMoveEvent.movementX
-        let moveY = mouseMoveEvent.movementY
+        let moveX = mouseMoveEvent.movementX;
+        let moveY = mouseMoveEvent.movementY;
         let result = null;
 
         if (Math.abs(moveX) > Math.abs(moveY)) {
@@ -50,22 +50,22 @@ export default class Board extends Component {
     }
 
     getTouchMove(touchMoveEvent, ongoingTouches) {
-        let newTouch = { 
+        let newTouch = {
             posX: touchMoveEvent.targetTouches[0].pageX,
             posY: touchMoveEvent.targetTouches[0].pageY,
-        } 
+        };
         ongoingTouches.push(newTouch);
         return ongoingTouches;
     }
 
-    evalTouches(ongoingTouches){
-        if(!ongoingTouches || ongoingTouches.length === 0) return;
+    evalTouches(ongoingTouches) {
+        if (!ongoingTouches || ongoingTouches.length === 0) return;
 
         let start = ongoingTouches[0];
-        let end = ongoingTouches[ongoingTouches.length-1];
-        let moveX = end.posX - start.posX
-        let moveY = end.posY - start.posY
-        
+        let end = ongoingTouches[ongoingTouches.length - 1];
+        let moveX = end.posX - start.posX;
+        let moveY = end.posY - start.posY;
+
         let result = null;
         if (Math.abs(moveX) > Math.abs(moveY)) {
             result = moveX > 0 ? "right" : "left";
@@ -80,34 +80,34 @@ export default class Board extends Component {
         if (!direction) return;
 
         let shiftToStart = (colOrRow, currentStones) => {
-            for( let k = 0; k < n-1; k = k + 1 ){
-                currentStones[colOrRow[k]] = currentStones[colOrRow[k+1]]
+            for (let k = 0; k < n - 1; k = k + 1) {
+                currentStones[colOrRow[k]] = currentStones[colOrRow[k + 1]];
             }
-            currentStones[colOrRow[n-1]] = 0;
+            currentStones[colOrRow[n - 1]] = 0;
             this.setState({ currentStones: currentStones });
-        }
+        };
 
         let shiftToEnd = (colOrRow, currentStones) => {
-            for( let k = n-1; k > 0; k = k - 1){
-                currentStones[colOrRow[k]] = currentStones[colOrRow[k-1]]
+            for (let k = n - 1; k > 0; k = k - 1) {
+                currentStones[colOrRow[k]] = currentStones[colOrRow[k - 1]];
             }
             currentStones[colOrRow[0]] = 0;
             this.setState({ currentStones: currentStones });
-        }
+        };
 
         if (direction === "up" || direction === "down") {
             let col = [];
             let number = fieldId % n;
             for (let i = n; i > 0; i = i - 1) {
-                col.push(n * (n - i) + number)
+                col.push(n * (n - i) + number);
             }
-            if( direction === "up" ){
-                if( currentStones[col[0]] === 1 ) return; 
+            if (direction === "up") {
+                if (currentStones[col[0]] === 1) return;
                 shiftToStart(col, currentStones);
                 return;
             }
-            if( direction === "down" ){
-                if(currentStones[col[n-1]] === 1 ) return; 
+            if (direction === "down") {
+                if (currentStones[col[n - 1]] === 1) return;
                 shiftToEnd(col, currentStones);
                 return;
             }
@@ -117,15 +117,15 @@ export default class Board extends Component {
             let row = [];
             let number = Math.floor(fieldId / n);
             for (let j = n; j > 0; j = j - 1) {
-                row.push((n - j) + number * n)
+                row.push(n - j + number * n);
             }
-            if( direction === "right"){
-                if( currentStones[row[n-1]] === 1 ) return; 
+            if (direction === "right") {
+                if (currentStones[row[n - 1]] === 1) return;
                 shiftToEnd(row, currentStones);
                 return;
             }
-            if( direction === "left"){
-                if( currentStones[row[0]] === 1 ) return; 
+            if (direction === "left") {
+                if (currentStones[row[0]] === 1) return;
                 shiftToStart(row, currentStones);
                 return;
             }
@@ -138,17 +138,25 @@ export default class Board extends Component {
         let direction = null;
 
         return fieldsArray.map((field, index) => (
-            <Field key={index} id={index} rows={rows} dark={field}
-                onMouseDown={() => mouseDown = true}
-                onMouseMove={mouseMoveEvent => direction = this.getMouseMove(mouseMoveEvent, mouseDown)}
+            <Field
+                key={index}
+                id={index}
+                rows={rows}
+                dark={field}
+                onMouseDown={() => (mouseDown = true)}
+                onMouseMove={mouseMoveEvent =>
+                    (direction = this.getMouseMove(mouseMoveEvent, mouseDown))
+                }
                 onMouseUp={() => {
-                    mouseDown = false
-                    this.checkMove(direction, index, rows, currentStones)
+                    mouseDown = false;
+                    this.checkMove(direction, index, rows, currentStones);
                 }}
-                onTouchMove={touchMoveEvent => ongoingTouches = this.getTouchMove(touchMoveEvent, ongoingTouches)}
+                onTouchMove={touchMoveEvent =>
+                    (ongoingTouches = this.getTouchMove(touchMoveEvent, ongoingTouches))
+                }
                 onTouchEnd={() => {
                     direction = this.evalTouches(ongoingTouches);
-                    this.checkMove(direction, index, rows, currentStones)
+                    this.checkMove(direction, index, rows, currentStones);
                     ongoingTouches = [];
                 }}
             >
@@ -157,18 +165,30 @@ export default class Board extends Component {
         ));
     }
 
-    render() {
-        let currentStones = this.state.currentStones.length === 0 ? 
-        this.initStones(this.props.numberOfRows) : this.state.currentStones;
-        let displayWin = false;
-        
-        if( JSON.stringify(currentStones) === JSON.stringify(this.props.fieldsArray) ){
-            displayWin = true;
+    checkIfWon(stones, fields) {
+        if (JSON.stringify(stones) === JSON.stringify(fields)) {
+            return true;
         }
+        return false;
+    }
 
-        return <PlayingBoard>
-            {this.getFields(this.props.fieldsArray, this.props.numberOfRows, currentStones)}
-            {displayWin ? <InfoBox><WinningText>YOU WIN!!</WinningText></InfoBox>: <></>}
-        </PlayingBoard>;
+    render() {
+        let currentStones =
+            this.state.currentStones.length === 0
+                ? this.initStones(this.props.numberOfRows)
+                : this.state.currentStones;
+
+        return (
+            <PlayingBoard>
+                {this.getFields(this.props.fieldsArray, this.props.numberOfRows, currentStones)}
+                {this.checkIfWon(currentStones, this.props.fieldsArray) ? (
+                    <InfoBox>
+                        <WinningText>YOU WIN!!</WinningText>
+                    </InfoBox>
+                ) : (
+                    <></>
+                )}
+            </PlayingBoard>
+        );
     }
 }
